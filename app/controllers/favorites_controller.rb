38878812b9aa645c230fd82745aa4cb6bc15762d
1013180 <hiroki.kojima0417@gmail.com>
2021-03-11 12:06:21 +0000

    いいね非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,8 @@
 class FavoritesController < ApplicationController
   def create
     @shoe = Shoe.find(params[:shoe_id])
-    favorite = current_user.favorites.new(shoe_id: @shoe.id)
-    favorite.save
+    @favorite = current_user.favorites.new(shoe_id: @shoe.id)
+    @favorite.save
   end
 
   def destroy
