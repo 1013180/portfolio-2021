71d9782b758300ad59e27e34916e0f0f88a8ff22
1013180 <hiroki.kojima0417@gmail.com>
2021-03-15 07:56:32 +0000

    topic_edit_view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attachment :profile_image, destroy: false
+
   has_many :shoes
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates :name, presence: true,
+                   uniqueness: true,
+                   length: { minimum: 2, maximum: 20 }
+  validates :introduction,
+                   length: { maximum: 50 }
 end
